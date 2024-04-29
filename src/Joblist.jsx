a8c24955a632fa95bc8c474d034e45e34b216992
Dicksen Tan <tandicksen@gmail.com>
2024-04-30 01:04:21 +0700

    Add class based on width

--- src/Joblist.jsx
+++ src/Joblist.jsx
@@ -1,12 +1,43 @@
 function Joblist(props){
-    const {data} = props
+    const {data, width} = props   
     return(
-        <article className="job_list">
+        <article className={`job_list ${data.featured ? 'new_border': ''}`}>
             <div className="job_details">
-                <img src="" alt="" className="job_profile" />
-                
+                <img src={data.logo} alt="" className="job_profile" />
+                <div className="job_description">
+                    <div className="company_status">
+                        <p className="company_name">
+                        {data.company}
+                        </p>
+                        <p className="new">
+
+                        </p>
+                        <p className="featured">
+                            
+                        </p>
+                    </div>
+                    <h4 className="job_title">
+                        {data.position}
+                    </h4>  
+                    <div className="job_status">
+                        <p className="post_data">{data.postedAt}</p>
+                        •	
+                        <p className="work_time">{data.contract}</p>
+                        •	
+                        <p className="location">{data.location}</p>
+                    </div>
+                </div>
             </div>
-            <div className="requirement"></div>
+            <div className={`requirement ${width < 1024 ? 'requirement_border' : ''}`}>
+                <p>{data.role}</p>
+                <p>{data.level}</p>
+                {data.languages.map((dl) =>(
+                    <p>{dl}</p>
+                ))}
+                {data.tools.map((dt)=>(
+                    <p>{dt}</p>
+                ))}
+            </div>
         </article>
     )
 }
